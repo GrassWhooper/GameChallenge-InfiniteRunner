501179aab7c6be435bee7394f071839bb90d55dc
GrassWhooper:Motasem <Mutrz@outlook.com>
2016-07-27 20:48:25 +0300

    Day 3 : 27/01/2016: More movment improvements and modifiying the second testing scene.

--- InfiniteRunner/Assets/Scripts/PlayerBehaviour.cs
+++ InfiniteRunner/Assets/Scripts/PlayerBehaviour.cs
@@ -2,11 +2,12 @@
 using System.Collections;
 using UnityStandardAssets.CrossPlatformInput;
 using System.Collections.Generic;
+using System.Linq;
 
 public class PlayerBehaviour : MonoBehaviour {
 
-    enum movementStatuses {NormalMovement , AcceleratedMovement  };
-
+    enum movementStatuses {NormalMovement , AcceleratedMovement, SlowedDownMovement  };
+    
     movementStatuses playerStatus;
 
     [Tooltip(" Force to be put into {X} Direction when moving ")]
@@ -24,87 +25,154 @@ public class PlayerBehaviour : MonoBehaviour {
     [Tooltip("this is applied when not moving above one, speed will increase")]
     public float speedMultiplierWhenNotMoving = 0.9f;
 
-    [Tooltip("how much faster to go on the Z direction")]
-    public float accelerateByOnZ = 20f;
+    [Tooltip("How Much To Multiply everything when accelerating ")]
+    public float accelerationMultiplier = 2f;
 
-    public float acceleratedMaxSpeed=80;
-
-    public bool accelerating = false;
-
-    float VertiDirecting;
+    [Tooltip("How Much To Multiply everything when slowing down{Suggested Between 0.001 and 0.999} ")]
+    public float slowDownMultiplier = 0.5f;
+        
     float xMovement;
     Rigidbody rb3d;
-    Vector3 movementDirection;
+    Vector3 BaseVector;
     float currentXSpeed;
-
-
+    float initalSpeed, initalMaxSpeed, initalAutoMovespeed, initalMaxAutoMoveSpeed;
+    [Tooltip("Select Keys For Each Action")]
+    public List<KeyCode> JumpKeys , AccelerateKeys, SlowDownKeys;
 
     // Use this for initialization
     void Start ()
     {
         rb3d = GetComponent<Rigidbody>();
-        movementDirection = new Vector3(1, 1, 1);
+        BaseVector = new Vector3(1, 1, 1);
         playerStatus = new movementStatuses();
         playerStatus = movementStatuses.NormalMovement;
+
+
+        initalSpeed = speed;
+        initalMaxSpeed = maxSpeed;
+        initalAutoMovespeed = autoMoveSpeed;
+        initalMaxAutoMoveSpeed = maxAutoMoveSpeed;
     }
-	
-	// Update is called once per frame
-	void Update ()
+
+    void Update()
     {
-        xPlayerMovement();
-        zPlayerMovement();
+        CheckForMovementStatus();
+    }
+
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        PlayerMovement();
         print(rb3d.velocity);
+    }
+    void ReSetMovementStatusValues()
+    {
+        speed = initalSpeed;
+        maxSpeed = initalMaxSpeed;
+        autoMoveSpeed = initalAutoMovespeed;
+        maxAutoMoveSpeed = initalMaxAutoMoveSpeed;
+    }
+    void SetMovementStatusValues()
+    {
+        if (playerStatus == movementStatuses.NormalMovement)
+        {
+            ReSetMovementStatusValues();
+        }
+        else if (playerStatus == movementStatuses.AcceleratedMovement)
+        {
+            speed = accelerationMultiplier * initalSpeed;
+            maxSpeed = accelerationMultiplier * initalMaxSpeed;
+            autoMoveSpeed = accelerationMultiplier * initalAutoMovespeed;
+            maxAutoMoveSpeed = accelerationMultiplier * initalMaxAutoMoveSpeed;
+        }
+        else if (playerStatus==movementStatuses.SlowedDownMovement)
+        {
+            speed = slowDownMultiplier * initalSpeed;
+            maxSpeed = slowDownMultiplier * initalMaxSpeed;
+            autoMoveSpeed = slowDownMultiplier * initalAutoMovespeed;
+            maxAutoMoveSpeed = slowDownMultiplier * initalMaxAutoMoveSpeed;
+        }
+
+    }
+
+    void CheckForMovementStatus()
+    {
+        bool Accelerating= false;
+        bool SlowingDown = false;
+
+        foreach (KeyCode item in AccelerateKeys)
+        {
+            if (Input.GetKey(item))
+            {
+                playerStatus = movementStatuses.AcceleratedMovement;
+                Accelerating = true;
+
+                break;
+            }
+            else
+            {
+                Accelerating = false;
+            }
+        }
+        foreach (KeyCode item in SlowDownKeys)
+        {
+            if (Input.GetKey(item))
+            {
+                playerStatus = movementStatuses.SlowedDownMovement;
+                SlowingDown = true;
+                break;
+            }
+            else
+            {
+                SlowingDown = false;
+            }
+        }
+        if (!Accelerating && !SlowingDown)
+        {
+            playerStatus = movementStatuses.NormalMovement;
+        }
     }
 
     void PlayerMovement()
     {
-
-
-    }
-
-    void zPlayerMovement()
-    {
-        VertiDirecting = CrossPlatformInputManager.GetAxis("Vertical");
-        Vector3 movementDirection = new Vector3(1, 1, 1);
+        xMovement = CrossPlatformInputManager.GetAxis("Horizontal");
         Vector3 movementForce = new Vector3(0, 0, 0);
+        Vector3 speedLimiter = new Vector3(0, 0, 0);
+        Vector3 speedDecrementer = new Vector3();
+
+        SetMovementStatusValues();
+
         if (Mathf.Abs(rb3d.velocity.z) < (maxAutoMoveSpeed))
         {
-            movementForce.z = movementForce.z + movementDirection.z;
+            movementForce.z = BaseVector.z;
             movementForce = movementForce * autoMoveSpeed;
 
             rb3d.AddForce(movementForce);
         }
-        else if (Mathf.Abs( rb3d.velocity.z )>= maxAutoMoveSpeed)
+        else if (Mathf.Abs(rb3d.velocity.z) >= maxAutoMoveSpeed)
         {
-            Vector3 speedLimiter = new Vector3(0, 0, 0);
-            speedLimiter.z = speedLimiter.z + movementDirection.z;
+            speedLimiter.z = BaseVector.z;
             speedLimiter = speedLimiter * maxAutoMoveSpeed;
+
             speedLimiter.x = rb3d.velocity.x;
 
             rb3d.velocity = speedLimiter;
         }
-    }
-
-
-    void xPlayerMovement()
-    {
-        xMovement = CrossPlatformInputManager.GetAxis("Horizontal");
-          
-        if (xMovement!=0)
+        movementForce = new Vector3(0, 0, 0); 
+        // {X} Movement Starts Here
+        if (xMovement != 0)
         {
-            if (Mathf.Abs(rb3d.velocity.x)<maxSpeed)
+            if (Mathf.Abs(rb3d.velocity.x) < maxSpeed)
             {
-                Vector3 movementForce = new Vector3(0,0,0);
-                movementForce.x = movementForce.x + movementDirection.x;
+                movementForce.x = BaseVector.x;
                 movementForce = movementForce * xMovement * speed;
 
                 rb3d.AddForce(movementForce);
                 currentXSpeed = rb3d.velocity.x;
             }
-            else if (Mathf.Abs(rb3d.velocity.x)>=maxSpeed)
+            else if (Mathf.Abs(rb3d.velocity.x) >= maxSpeed)
             {
-                Vector3 speedLimiter = new Vector3();
-                speedLimiter.x = movementDirection.x;
+                speedLimiter.x = BaseVector.x;
                 speedLimiter.x = speedLimiter.x * maxSpeed * xMovement;
 
                 speedLimiter.z = rb3d.velocity.z;
@@ -113,11 +181,11 @@ public class PlayerBehaviour : MonoBehaviour {
 
                 currentXSpeed = rb3d.velocity.x;
             }
+           
         }
-        if (xMovement==0)
+        if (xMovement == 0)
         {
-            Vector3 speedDecrementer = new Vector3();
-            speedDecrementer.x = movementDirection.x;
+            speedDecrementer.x = BaseVector.x;
             speedDecrementer.x = currentXSpeed * speedMultiplierWhenNotMoving;
 
             speedDecrementer.z = rb3d.velocity.z;
@@ -126,6 +194,5 @@ public class PlayerBehaviour : MonoBehaviour {
             rb3d.velocity = speedDecrementer;
 
         }
-  
     }
 }
